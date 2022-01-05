f7ce3471e405f946165fbe3cf04aaf797295b764
tusharvaswanicoder <codewithtusharvaswani@gmail.com>
2022-01-05 07:23:03 +0530

    commited

---
+++ src/Components/HourData.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+const HourDataContainer = styled.div``;
+
+const HourWeatherIcon = styled.img``;
+
+const HourTemperature = styled.div``;
+
+const HourExtras = styled.div``;
+
+const HourHumidity = styled.div``;
+
+const HourWindSpeed = styled.div``;
+
+const HourWeatherStatus = styled.div``;
+
+const HourWeatherDetail = styled.div``;
+
+const HourOfWeatherTime = styled.div``;
+
+function HourData({ weather_of_hour, unit }) {
+    console.log(weather_of_hour);
+    return (
+        <HourDataContainer>
+            <HourWeatherDetail>
+                <HourWeatherIcon
+                    src={weather_of_hour.condition.icon}
+                ></HourWeatherIcon>
+                <HourTemperature>
+                    {unit === "C"
+                        ? `${Math.trunc(weather_of_hour.temp_c)}`
+                        : `${Math.trunc(weather_of_hour.temp_f)}`}
+                    °
+                </HourTemperature>
+                <HourWeatherStatus>
+                    {weather_of_hour.condition.text}
+                </HourWeatherStatus>
+            </HourWeatherDetail>
+            <HourOfWeatherTime>
+                {new Date(weather_of_hour.time_epoch * 1000).getHours() >= 12
+                    ? `${
+                          24 -
+                          new Date(weather_of_hour.time_epoch * 1000).getHours()
+                      } pm`
+                    : `${new Date(
+                          weather_of_hour.time_epoch * 1000
+                      ).getHours()} am`}
+            </HourOfWeatherTime>
+        </HourDataContainer>
+    );
+}
+
+export default HourData;
--- src/Components/HourlyData.jsx
+++ src/Components/HourlyData.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import HourData from "./HourData";
+
+const HourlyDataContainer = styled.div`
+    padding: 20px 0px 40px 0px;
+`;
+
+const HourlyDataHeading = styled.div`
+    font-size: 25px;
+    margin-bottom: 20px;
+`;
+
+const HourlyDataContent = styled.div`
+    display: flex;
+    overflow-x: hidden;
+    padding-bottom: 20px;
+`;
 
 function HourlyData({ realtime_weather, unit, selectedDay }) {
-    return <div></div>;
+    return (
+        <HourlyDataContainer>
+            <HourlyDataHeading>Hourly Data</HourlyDataHeading>
+            <HourlyDataContent>
+                {realtime_weather.forecast.forecastday[selectedDay].hour.map(
+                    function (weather_of_hour) {
+                        return (
+                            <HourData
+                                weather_of_hour={weather_of_hour}
+                                unit={unit}
+                            />
+                        );
+                    }
+                )}
+            </HourlyDataContent>
+        </HourlyDataContainer>
+    );
 }
 
 export default HourlyData;
